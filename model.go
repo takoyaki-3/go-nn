5cfeff276b3096d46b638713192dec5bea01f61c
takoyaki-3 <mail@takoyaki3.com>
2022-10-20 19:02:28 +0900

    整形

--- model.go
+++ model.go
@@ -10,47 +10,47 @@ import (
 )
 
 type NeuralNetwork struct {
-	inputNodes int	// 入力ノード数
-	hiddenNodes int	// 隠れ層ノード数
-	outputNodes int	// 出力ノード数
-	wi *mat.Dense		// 入力層と隠れ層間の重み
-	wo *mat.Dense		// 隠れ層と出力層間の重み
-	Score float64		// 得点
+	inputNodes  int        // 入力ノード数
+	hiddenNodes int        // 隠れ層ノード数
+	outputNodes int        // 出力ノード数
+	wi          *mat.Dense // 入力層と隠れ層間の重み
+	wo          *mat.Dense // 隠れ層と出力層間の重み
+	Score       float64    // 得点
 }
 
 const RAND = 10000
 
-func NewRandom05to05Dense(r,c int) *mat.Dense {
+func NewRandom05to05Dense(r, c int) *mat.Dense {
 	data := []float64{}
-	for i:=0;i<r*c;i++{
-		data = append(data, float64(rand.Intn(RAND)) / float64(RAND) - 0.5)
+	for i := 0; i < r*c; i++ {
+		data = append(data, float64(rand.Intn(RAND))/float64(RAND)-0.5)
 	}
 
 	return mat.NewDense(r, c, data)
 }
 
 // 乱数を初期値とした新規ニューラルネットワーク作成
-func NewNeuralNetwork(inputNodes, outputNodes, hiddenNodes int)*NeuralNetwork{
+func NewNeuralNetwork(inputNodes, outputNodes, hiddenNodes int) *NeuralNetwork {
 	nn := new(NeuralNetwork)
 	nn.inputNodes = inputNodes
 	nn.hiddenNodes = hiddenNodes
 	nn.outputNodes = outputNodes
 
-	nn.wi = NewRandom05to05Dense(hiddenNodes,inputNodes)
-	nn.wo = NewRandom05to05Dense(outputNodes,hiddenNodes)
+	nn.wi = NewRandom05to05Dense(hiddenNodes, inputNodes)
+	nn.wo = NewRandom05to05Dense(outputNodes, hiddenNodes)
 
 	return nn
 }
 
 // 親ニューラルネットワークから子ニューラルネットワークを作成
-func Parent2Child(p []*NeuralNetwork,mutation float64)*NeuralNetwork{
+func Parent2Child(p []*NeuralNetwork, mutation float64) *NeuralNetwork {
 	nn := new(NeuralNetwork)
 	nn.inputNodes = p[0].inputNodes
 	nn.hiddenNodes = p[0].hiddenNodes
 	nn.outputNodes = p[0].outputNodes
 
-	nn.wi = mat.NewDense(nn.hiddenNodes,nn.inputNodes,nil)
-	nn.wo = mat.NewDense(nn.outputNodes,nn.hiddenNodes,nil)
+	nn.wi = mat.NewDense(nn.hiddenNodes, nn.inputNodes, nil)
+	nn.wo = mat.NewDense(nn.outputNodes, nn.hiddenNodes, nil)
 	// fmt.Println(nn.wo.ColView(0).Len())
 	// fmt.Println(nn.wo.RowView(0).Len())
 
@@ -58,33 +58,33 @@ func Parent2Child(p []*NeuralNetwork,mutation float64)*NeuralNetwork{
 	ps = append(ps, rand.Intn(len(p)))
 	ps = append(ps, rand.Intn(len(p)))
 
-	for i:=0;i<nn.hiddenNodes;i++{
-		for j:=0;j<nn.inputNodes;j++{
+	for i := 0; i < nn.hiddenNodes; i++ {
+		for j := 0; j < nn.inputNodes; j++ {
 			// fmt.Println(i,j,"inputNodes")
 			// fmt.Println(nn.wi.ColView(0).Len())
 			// fmt.Println(nn.wi.RowView(0).Len())
-			if float64(rand.Intn(RAND)) / float64(RAND) > mutation {
+			if float64(rand.Intn(RAND))/float64(RAND) > mutation {
 				t := ps[rand.Intn(len(ps))]
-				x := p[t].wi.At(i,j)
-				nn.wi.Set(i,j,x)
+				x := p[t].wi.At(i, j)
+				nn.wi.Set(i, j, x)
 			} else {
-				x := float64(rand.Intn(RAND)) / float64(RAND) - 0.5
-				nn.wi.Set(i,j,x)
+				x := float64(rand.Intn(RAND))/float64(RAND) - 0.5
+				nn.wi.Set(i, j, x)
 			}
 		}
 	}
-	for i:=0;i<nn.outputNodes;i++{
-		for j:=0;j<nn.hiddenNodes;j++{
+	for i := 0; i < nn.outputNodes; i++ {
+		for j := 0; j < nn.hiddenNodes; j++ {
 			// fmt.Println(i,j,"outputNodes")
 			// fmt.Println(nn.wo.ColView(0).Len())
 			// fmt.Println(nn.wo.RowView(0).Len())
-			if float64(rand.Intn(RAND)) / float64(RAND) > mutation {
+			if float64(rand.Intn(RAND))/float64(RAND) > mutation {
 				t := ps[rand.Intn(len(ps))]
-				x := p[t].wo.At(i,j)
-				nn.wo.Set(i,j,x)
+				x := p[t].wo.At(i, j)
+				nn.wo.Set(i, j, x)
 			} else {
-				x := float64(rand.Intn(RAND)) / float64(RAND+1) - 1.0
-				nn.wo.Set(i,j,x)
+				x := float64(rand.Intn(RAND))/float64(RAND+1) - 1.0
+				nn.wo.Set(i, j, x)
 			}
 		}
 	}
@@ -93,9 +93,9 @@ func Parent2Child(p []*NeuralNetwork,mutation float64)*NeuralNetwork{
 }
 
 // ニューラルネットワークを用いて入力ベクトルから出力ベクトルを得る
-func (nn *NeuralNetwork)Query(input *mat.Dense)*mat.Dense{
-	A := mat.NewDense(nn.hiddenNodes,1,nil)
-	A.Product(nn.wi,input)
+func (nn *NeuralNetwork) Query(input *mat.Dense) *mat.Dense {
+	A := mat.NewDense(nn.hiddenNodes, 1, nil)
+	A.Product(nn.wi, input)
 
 	// 要素ごとに適用する関数
 	sigmoid := func(i, j int, v float64) float64 {
@@ -104,8 +104,8 @@ func (nn *NeuralNetwork)Query(input *mat.Dense)*mat.Dense{
 	var B mat.Dense
 	B.Apply(sigmoid, A)
 
-	C := mat.NewDense(nn.outputNodes,1,nil)
-	C.Product(nn.wo,&B)
+	C := mat.NewDense(nn.outputNodes, 1, nil)
+	C.Product(nn.wo, &B)
 
 	var D mat.Dense
 	D.Apply(sigmoid, C)
@@ -115,62 +115,62 @@ func (nn *NeuralNetwork)Query(input *mat.Dense)*mat.Dense{
 
 // ニューラルネットワークの一時保存用JSONデータ形式
 type Data struct {
-	InputNodes int `json:"input_nodes"`
-	HiddenNodes int `json:"hidden_nodes"`
-	OutputNodes int `json:"output_nodes"`
-	Wi [][]float64 `json:"wi"`
-	Wo [][]float64 `json:"wo"`
-	Score float64 `json:"score"`
+	InputNodes  int         `json:"input_nodes"`
+	HiddenNodes int         `json:"hidden_nodes"`
+	OutputNodes int         `json:"output_nodes"`
+	Wi          [][]float64 `json:"wi"`
+	Wo          [][]float64 `json:"wo"`
+	Score       float64     `json:"score"`
 }
 
 // ニューラルネットワークをJSON形式で保存
-func (nn *NeuralNetwork)Save(path string)error{
-	wi := make([][]float64,nn.hiddenNodes)
-	for i:=0;i<nn.hiddenNodes;i++{
-		for j:=0;j<nn.inputNodes;j++{
-			x := nn.wi.At(i,j)
+func (nn *NeuralNetwork) Save(path string) error {
+	wi := make([][]float64, nn.hiddenNodes)
+	for i := 0; i < nn.hiddenNodes; i++ {
+		for j := 0; j < nn.inputNodes; j++ {
+			x := nn.wi.At(i, j)
 			wi[i] = append(wi[i], x)
 		}
 	}
-	wo := make([][]float64,nn.outputNodes)
-	for i:=0;i<nn.outputNodes;i++{
-		for j:=0;j<nn.hiddenNodes;j++{
-			x := nn.wo.At(i,j)
+	wo := make([][]float64, nn.outputNodes)
+	for i := 0; i < nn.outputNodes; i++ {
+		for j := 0; j < nn.hiddenNodes; j++ {
+			x := nn.wo.At(i, j)
 			wo[i] = append(wo[i], x)
 		}
 	}
 	data := Data{
-		InputNodes: nn.inputNodes,
+		InputNodes:  nn.inputNodes,
 		HiddenNodes: nn.hiddenNodes,
 		OutputNodes: nn.outputNodes,
-		Wi: wi,
-		Wo: wo,
-		Score: nn.Score,
+		Wi:          wi,
+		Wo:          wo,
+		Score:       nn.Score,
 	}
-	return json.DumpToFile(&data,path)
+	return json.DumpToFile(&data, path)
 }
 
 // ニューラルネットワークのJSONファイルを読み込み
-func (nn *NeuralNetwork)Load(path string)error{
+func (nn *NeuralNetwork) Load(path string) error {
 	var data Data
-	if err:=json.LoadFromPath(path,&data);err!=nil{
+	if err := json.LoadFromPath(path, &data); err != nil {
 		return err
 	}
 	nn.hiddenNodes = data.HiddenNodes
 	nn.inputNodes = data.InputNodes
 	nn.outputNodes = data.OutputNodes
 
-	nn.wi = mat.NewDense(nn.hiddenNodes,nn.inputNodes,nil)
-	nn.wo = mat.NewDense(nn.outputNodes,nn.hiddenNodes,nil)
+	nn.wi = mat.NewDense(nn.hiddenNodes, nn.inputNodes, nil)
+	nn.wo = mat.NewDense(nn.outputNodes, nn.hiddenNodes, nil)
 
-	for i:=0;i<nn.hiddenNodes;i++{
-		for j:=0;j<nn.inputNodes;j++{
-			nn.wi.Set(i,j,data.Wi[i][j])
+	for i := 0; i < nn.hiddenNodes; i++ {
+		for j := 0; j < nn.inputNodes; j++ {
+			nn.wi.Set(i, j, data.Wi[i][j])
 		}
 	}
-	for i:=0;i<nn.outputNodes;i++{
-		for j:=0;j<nn.hiddenNodes;j++{
-			nn.wo.Set(i,j,data.Wo[i][j])
+	for i := 0; i < nn.outputNodes; i++ {
+		for j := 0; j < nn.hiddenNodes; j++ {
+			nn.wo.Set(i, j, data.Wo[i][j])
 		}
 	}
 	return nil
--- sample/main.go
+++ sample/main.go
@@ -13,27 +13,27 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-func loadFile(fname string, inputNodeSize, outputNodeSize int)([]*mat.Dense, []*mat.Dense){
-	inputs := make([]*mat.Dense,0)
-	outputs := make([]*mat.Dense,0)
-	
+func loadFile(fname string, inputNodeSize, outputNodeSize int) ([]*mat.Dense, []*mat.Dense) {
+	inputs := make([]*mat.Dense, 0)
+	outputs := make([]*mat.Dense, 0)
+
 	df := goc.Read2DStr(fname)
 
-	for _,line := range df{
-		input := mat.NewDense(len(line)-1,1,nil)
-		output := mat.NewDense(10,1,nil)
-		x,_ := strconv.Atoi(line[0])
-		output.Set(x,0,0.999)
-	
-		for i,n:=range line[1:] {
-			x,_:=strconv.Atoi(n)
-			input.Set(i,0,float64(x)/255.0+0.001)
+	for _, line := range df {
+		input := mat.NewDense(len(line)-1, 1, nil)
+		output := mat.NewDense(10, 1, nil)
+		x, _ := strconv.Atoi(line[0])
+		output.Set(x, 0, 0.999)
+
+		for i, n := range line[1:] {
+			x, _ := strconv.Atoi(n)
+			input.Set(i, 0, float64(x)/255.0+0.001)
 		}
-		inputs = append(inputs,input)
-		outputs = append(outputs,output)
+		inputs = append(inputs, input)
+		outputs = append(outputs, output)
 	}
 
-	return inputs,outputs
+	return inputs, outputs
 }
 
 func matPrint(X mat.Matrix) {
@@ -41,9 +41,9 @@ func matPrint(X mat.Matrix) {
 	fmt.Printf("%v\n", fa)
 }
 
-func NewDense(r,c int,x float64) *mat.Dense {
+func NewDense(r, c int, x float64) *mat.Dense {
 	data := []float64{}
-	for i:=0;i<r*c;i++{
+	for i := 0; i < r*c; i++ {
 		data = append(data, x)
 	}
 
@@ -54,26 +54,25 @@ const NumParent = 20
 
 const NumCore = 8
 
-func main(){
+func main() {
 	fmt.Println("hello")
 
-	inputs,outputs := loadFile("./sample_data/mnist_train_small.csv",784,10)
+	inputs, outputs := loadFile("./sample_data/mnist_train_small.csv", 784, 10)
 
 	nns := []*gonn.NeuralNetwork{}
-	for i:=0;i<NumParent;i++{
-		nn := gonn.NewNeuralNetwork(784,10,200)
+	for i := 0; i < NumParent; i++ {
+		nn := gonn.NewNeuralNetwork(784, 10, 200)
 		nns = append(nns, nn)
 	}
 
 	// 現状出力
 	fmt.Println("-----------------")
-	for _,nn:=range nns{
-		fmt.Print(nn.Score," ")
+	for _, nn := range nns {
+		fmt.Print(nn.Score, " ")
 	}
 	fmt.Println()
 
-
-	for e := 0;e<50000;e+=100{
+	for e := 0; e < 50000; e += 100 {
 
 		in := []*mat.Dense{}
 		out := []*mat.Dense{}
@@ -82,52 +81,51 @@ func main(){
 
 		NumChild := 200
 		er := 0.01
-		if e % 10 == 9{
+		if e%10 == 9 {
 			NumChild = 500
 			er = 0.02
 		}
 
-		nnsSub := make([][]*gonn.NeuralNetwork,NumCore)
+		nnsSub := make([][]*gonn.NeuralNetwork, NumCore)
 
 		n := e
-		if n > len(inputs){
-			n=len(inputs)
+		if n > len(inputs) {
+			n = len(inputs)
 		}
 		in = inputs[:n]
 		out = outputs[:n]
 
-
 		wg := sync.WaitGroup{}
 		wg.Add(NumCore)
-		for rank:=0;rank<NumCore;rank++{
-			go func(rank int){
+		for rank := 0; rank < NumCore; rank++ {
+			go func(rank int) {
 				defer wg.Done()
-				for i:=rank;i<NumChild;i+=NumCore{
-					nn := gonn.Parent2Child(p,er)
-					nn.Score = Try(nn,in,out)
+				for i := rank; i < NumChild; i += NumCore {
+					nn := gonn.Parent2Child(p, er)
+					nn.Score = Try(nn, in, out)
 					nnsSub[rank] = append(nnsSub[rank], nn)
 				}
 			}(rank)
 		}
 		wg.Wait()
 		nns = []*gonn.NeuralNetwork{}
-		for rank:=0;rank<NumCore;rank++{
+		for rank := 0; rank < NumCore; rank++ {
 			nns = append(nns, nnsSub[rank]...)
 		}
 
-		for i,_:=range p[:2]{
-			p[i].Score = Try(p[i],in,out)
+		for i, _ := range p[:2] {
+			p[i].Score = Try(p[i], in, out)
 		}
 		nns = append(nns, p[:2]...)
-		sort.Slice(nns,func(i, j int) bool {
+		sort.Slice(nns, func(i, j int) bool {
 			return nns[i].Score > nns[j].Score
 		})
 
 		// 現状出力
 		fmt.Println("-----------------")
-		fmt.Println(e,":")
-		for _,nn:=range nns{
-			fmt.Print(nn.Score," ")
+		fmt.Println(e, ":")
+		for _, nn := range nns {
+			fmt.Print(nn.Score, " ")
 		}
 		fmt.Println()
 	}
@@ -137,21 +135,21 @@ func main(){
 	}
 }
 
-func Try(nn *gonn.NeuralNetwork,inputs,outputs []*mat.Dense)float64{
+func Try(nn *gonn.NeuralNetwork, inputs, outputs []*mat.Dense) float64 {
 	// 評価部分
-	var ok,ng int
-	for ii:=0;ii<len(inputs);ii++{
+	var ok, ng int
+	for ii := 0; ii < len(inputs); ii++ {
 		i := ii
 		ans := 0
-		for j:=1;j<10;j++{
-			if outputs[i].At(j,0) > outputs[i].At(ans,0) {
+		for j := 1; j < 10; j++ {
+			if outputs[i].At(j, 0) > outputs[i].At(ans, 0) {
 				ans = j
 			}
 		}
 		output := nn.Query(inputs[i])
 		most := 0
-		for j:=1;j<10;j++{
-			if output.At(j,0) > output.At(most,0) {
+		for j := 1; j < 10; j++ {
+			if output.At(j, 0) > output.At(most, 0) {
 				most = j
 			}
 		}
@@ -161,5 +159,5 @@ func Try(nn *gonn.NeuralNetwork,inputs,outputs []*mat.Dense)float64{
 			ng++
 		}
 	}
-	return float64(ok)/float64(ok+ng)
+	return float64(ok) / float64(ok+ng)
 }
